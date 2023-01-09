817606e67f19d31d9a86b9c49b9d11d51584cf83
Philipp Mayer <phlppmayer@gmail.com>
2023-01-09 16:24:34 +0100

    introduce legacy

--- src/main/kotlin/com/example/fcis/DealService.kt
+++ src/main/kotlin/com/example/fcis/DealService.kt
@@ -3,6 +3,8 @@ package com.example.fcis
 import io.micrometer.core.instrument.Counter
 import io.micrometer.core.instrument.MeterRegistry
 import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneOffset
 
 class DealService(
     private val orderRepository: OrderRepository,
@@ -13,29 +15,54 @@ class DealService(
     fun process(deliveryUpdate: DeliveryUpdate) {
         val order = orderRepository.findByIdOrNull(deliveryUpdate.orderId)
 
-        when (val updateResult = order.process(deliveryUpdate, now)) {
-            is UnknownUpdate -> {
-                monitor(updateResult.eventName)
-                log("No order with id ${deliveryUpdate.orderId} in database!")
-            }
-
-            is OutdatedUpdate -> {
-                monitor(updateResult.eventName)
-                log("Incoming update ${deliveryUpdate.id} is outdated! Ignoring it.")
-            }
-
-            is SuccessfullyUpdated -> {
-                orderRepository.update(updateResult.updatedOrder)
-
-                if (updateResult.email != null) {
-                    emailSystem.send(updateResult.email)
-                }
-
-                monitor(updateResult.eventName)
-                log("Processed update ${deliveryUpdate.id}")
-            }
+        if (order == null) {
+            log("No order with id ${deliveryUpdate.orderId} in database!")
+            monitor("updates.unknown")
+            return
         }
+
+        if (deliveryUpdate.isOlderThan(order.currentStatusDetails)) {
+            log("Incoming update ${deliveryUpdate.id} is outdated! Ignoring it.")
+            monitor("updates.outdated")
+            return
+        }
+
+        orderRepository.update(order.updateWith(deliveryUpdate, now))
+
+        if (order.customer.emailNotificationsEnabled) {
+            emailSystem.send(statusUpdateEmailOf(order))
+        }
+
+        monitor("updates.successful")
+        log("Processed update ${deliveryUpdate.id}")
     }
+
+
+    private fun DeliveryUpdate.isOlderThan(currentStatusDetails: Order.StatusDetails): Boolean =
+        statusUpdatedAt.toUTC().isBefore(currentStatusDetails.updatedAt.toUTC())
+
+    private fun Instant.toUTC(): LocalDateTime =
+        atOffset(ZoneOffset.UTC).toLocalDateTime()
+
+    private fun statusUpdateEmailOf(order: Order) = with(order) {
+        Email(
+            recipient = customer.emailAddress,
+            topic = "Your order $orderId has changed it's status!",
+            body = """
+               Hi,
+               Your order $orderId has changed it's status to ${currentStatusDetails.currentStatus}.
+               Check the website for more info.
+               """.trimIndent()
+        )
+    }
+
+    private fun Order.updateWith(deliveryUpdate: DeliveryUpdate, now: () -> Instant) = copy(
+        currentStatusDetails = Order.StatusDetails(
+            currentStatus = deliveryUpdate.newStatus,
+            updatedAt = now()
+        )
+    )
+
 
     private fun monitor(eventName: String) {
         Counter.builder(eventName)
